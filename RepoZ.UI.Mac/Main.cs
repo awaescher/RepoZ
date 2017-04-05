b5aec13680e0826ffc4115b4f4c17d63538d6c0f
awaescher <awaescher@gmx.de>
2017-04-06 00:09:44 +0200

    * RepoZ.sln: add XamMac2-project for Eto.Forms on MacOSX
    * MainForm.cs:
    * Main.cs:
    * Info.plist:
    * Program.cs:
    * AppDelegate.cs:
    * packages.config:
    * Main.storyboard:
    * ViewController.cs:
    * Entitlements.plist:
    * MacPathNavigator.cs:
    * RepoZ.UI.Mac.csproj:
    * MacRepositoryMonitor.cs:
    * ViewController.designer.cs:
    * RepoZ.UI.Desktop.csproj:
    * Contents.json:
    * Contents.json:
    * AppIcon-32.png:
    * AppIcon-16.png:
    * AppIcon-512.png:
    * AppIcon-128.png:
    * AppIcon-256.png:
    * AppIcon-16@2x.png:
    * AppIcon-32@2x.png:
    * AppIcon-128@2x.png:
    * AppIcon-256@2x.png:
    * AppIcon-512@2x.png:

---
+++ RepoZ.UI.Mac/Main.cs
@@ -0,0 +1,18 @@
+﻿using AppKit;
+using Eto;
+using Eto.Forms;
+
+namespace RepoZ.UI.Mac
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			new Application(Platform.Detect)
+				.Run(new MainForm(
+					new MacRepositoryMonitor(),
+					new MacPathNavigator()
+				));
+		}
+	}
+}
