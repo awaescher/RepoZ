7d1acc8076bf5542098f4b39b0a00ac0920a8d23
awaescher <awaescher@gmx.de>
2017-04-14 22:46:41 +0200

    Add MacOSX native reference Native binaries for libgit2sharp have to be added as Native References to get them included to the .app file after build.

--- RepoZ.Api.Mac/Git/MacRepositoryReader.cs
+++ RepoZ.Api.Mac/Git/MacRepositoryReader.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Linq;
+using LibGit2Sharp;
 using RepoZ.Api.Git;
 
 namespace RepoZ.Api.Mac.Git
@@ -7,19 +8,56 @@ namespace RepoZ.Api.Mac.Git
 	{
 		public Api.Git.Repository ReadRepository(string path)
 		{
-			var r = new LibGit2Sharp.Repository(path);
+			if (string.IsNullOrEmpty(path))
+				return Api.Git.Repository.Empty;
 
+			string repoPath = LibGit2Sharp.Repository.Discover(path);
+			if (string.IsNullOrEmpty(repoPath))
+				return Api.Git.Repository.Empty;
 
-			// TODO
-			return new Api.Git.Repository()
-			{
-				Name = "Repo",
-				Path = path,
-				CurrentBranch = "Repobranch",
-				AheadBy = 1,
-				BehindBy = 1
-			};
+			return ReadRepositoryWithRetries(repoPath, 3);
+
 		}
 
+		private Api.Git.Repository ReadRepositoryWithRetries(string repoPath, int maxRetries)
+		{
+			Api.Git.Repository repository = null;
+			int currentTry = 1;
+
+			while (repository == null && currentTry <= maxRetries)
+			{
+				try
+				{
+					repository = ReadRepositoryInternal(repoPath);
+				}
+				catch (LockedFileException)
+				{
+					if (currentTry >= maxRetries)
+						throw;
+					else
+						System.Threading.Thread.Sleep(500);
+				}
+
+				currentTry++;
+			}
+
+			return repository;
+		}
+
+		private Api.Git.Repository ReadRepositoryInternal(string repoPath)
+		{
+			using (var repo = new LibGit2Sharp.Repository(repoPath))
+			{
+				return new Api.Git.Repository()
+				{
+					Name = new System.IO.DirectoryInfo(repo.Info.WorkingDirectory).Name,
+					Path = repo.Info.WorkingDirectory,
+					Branches = repo.Branches.Select(b => b.FriendlyName).ToArray(),
+					CurrentBranch = repo.Head.FriendlyName,
+					AheadBy = repo.Head.TrackingDetails?.AheadBy ?? -1,
+					BehindBy = repo.Head.TrackingDetails?.BehindBy ?? -1
+				};
+			}
+		}
 	}
 }
