066359318cee70cf94d81860b37dab7de681469e
awaescher <awaescher@gmx.de>
2017-04-27 00:45:31 +0200

    Add doubleclick and context menu to the repository list

--- RepoZ.UI.Win.Wpf/MainWindow.xaml.cs
+++ RepoZ.UI.Win.Wpf/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace RepoZ.UI.Win.Wpf
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private IRepositoryActionProvider _repositoryActionProvider;
+
 		public MainWindow()
 		{
 			InitializeComponent();
@@ -29,7 +31,67 @@ namespace RepoZ.UI.Win.Wpf
 			var aggregator = TinyIoCContainer.Current.Resolve<IRepositoryInformationAggregator>();
 			lstRepositories.ItemsSource = aggregator.Repositories;
 
+			_repositoryActionProvider = TinyIoCContainer.Current.Resolve<IRepositoryActionProvider>();
+
 			lstRepositories.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription(nameof(RepositoryView.Name), System.ComponentModel.ListSortDirection.Ascending));
+		}
+
+		private void lstRepositories_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+		{
+			var repo = lstRepositories.SelectedItem as RepositoryView;
+			if (repo == null || !repo.WasFound)
+				return;
+
+			var action = _repositoryActionProvider.GetFor(repo.Repository)
+						 .FirstOrDefault(a => a.IsDefault);
+
+			action?.Action?.Invoke(sender, e);
+		}
+
+		private void lstRepositories_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+		{
+			var repo = lstRepositories.SelectedItem as RepositoryView;
+			if (repo == null || !repo.WasFound)
+			{
+				e.Handled = true;
+				return;
+			}
+
+			var items = new List<MenuItem>();
+
+			foreach (var action in _repositoryActionProvider.GetFor(repo.Repository))
+			{
+				//if (action.BeginGroup && items.Any())
+				//	items.Add(new SeparatorMenuItem());
+
+				items.Add(CreateMenuItem(sender, action));
+			}
+
+			FrameworkElement fe = e.Source as FrameworkElement;
+			fe.ContextMenu.ItemsSource = items;
+		}
+
+		private MenuItem CreateMenuItem(object sender, RepositoryAction action)
+		{
+			Action clickAction = () =>
+			{
+				if (action?.Action != null)
+					action.Action(null, null);
+			};
+
+			var item = new MenuItem()
+			{
+				Header = action.Name,
+				IsEnabled = action.CanExecute
+			};
+
+			if (action.SubActions != null)
+			{
+				foreach (var subAction in action.SubActions)
+					item.Items.Add(CreateMenuItem(sender, subAction));
+			}
+
+			return item;
 		}
 	}
 }
