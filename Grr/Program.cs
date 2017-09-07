2a29e46b5aba00e9ae99d9ca2e96eae36ed450f2
awaescher <awaescher@gmx.de>
2017-09-07 23:08:33 +0200

    Add Grr console project

---
+++ Grr/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Grr
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
