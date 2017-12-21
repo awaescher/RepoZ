d0454283f23b94149730e790ae8e39a111f70ccc
Andreas Wäscher <andreas.waescher@chg-meridian.de>
2017-12-21 12:44:39 +0100

    Introduce option --elevated to open files with an UAC prompt

--- Grr/Messages/OpenFileMessage.cs
+++ Grr/Messages/OpenFileMessage.cs
@@ -17,9 +17,47 @@ namespace grr.Messages
 			foreach (var file in files)
 			{
 				System.Console.WriteLine($"Opening {file} ...");
-				var psi = new ProcessStartInfo(file);
-				Process.Start(psi);
+
+				try
+				{
+					Process.Start(CreateStartInfo(file));
+				}
+				catch (System.Exception ex)  
+				{
+					System.Console.WriteLine("An error occured:\n" + ex.ToString());
+				}
 			}
+		}
+
+		private ProcessStartInfo CreateStartInfo(string file)
+		{
+			if (!Filter.RequestElevation)
+				return new ProcessStartInfo(file);
+
+			var isExecutable = IsExecutable(file);
+
+			// executables can be used directly, whereas files such as *.sln, for example,
+			// have to been opened with (a hidden) cmd.exe to request elevation.
+			// command is:   cmd.exe /C "C:\Path\Solution.sln"
+			var executable = isExecutable ? file : "cmd.exe";
+			var arguments = isExecutable ? "" : $"/C \"{file}\"";
+			var windowStyle = isExecutable ? ProcessWindowStyle.Normal : ProcessWindowStyle.Hidden;
+
+			return new ProcessStartInfo
+			{
+				UseShellExecute = true,		// this with Verb=runas forces elevation
+				CreateNoWindow = true,
+				WindowStyle = windowStyle,
+				FileName = executable,
+				Verb = "runas",				// this with ShellEx=true forces elevation
+				Arguments = arguments
+			};
+		}
+
+		private bool IsExecutable(string file)
+		{
+			var executables = new string[] { ".exe", ".bat", ".cmd", ".com" };
+			return executables.Any(e => file.EndsWith(e, System.StringComparison.OrdinalIgnoreCase));
 		}
 	}
 }
--- Grr/RepositoryFilterOptions.cs
+++ Grr/RepositoryFilterOptions.cs
@@ -13,6 +13,9 @@ namespace grr
 		[Option('r', "recursive", DefaultValue = false, HelpText = "Defines whether the file filter should be applied recursively or not.")]
 		public bool RecursiveFileFilter { get; set; }
 
+		[Option('e', "elevated", DefaultValue = false, HelpText = "Defines whether the files should be opened in an elevated context or not.")]
+		public bool RequestElevation { get; set; }
+
 		public bool HasFileFilter => !string.IsNullOrWhiteSpace(FileFilter);
 	}
 }
