21ae5d8a19874b6782309874e7fa82e1794caa44
awaescher <awaescher@gmx.de>
2018-04-20 22:43:25 +0200

    Enumerate drives in macOS

--- RepoZ.Api.Mac/IO/MacDriveEnumerator.cs
+++ RepoZ.Api.Mac/IO/MacDriveEnumerator.cs
@@ -4,12 +4,15 @@ using System.Linq;
 
 namespace RepoZ.Api.Mac.IO
 {
-	public class MacDriveEnumerator : IPathProvider
-	{
-		public string[] GetPaths()
-		{
-			// TODO
-			return new string[] { "/Users/andreaswascher/develop/" };
-		}
-	}
+    public class MacDriveEnumerator : IPathProvider
+    {
+        public string[] GetPaths()
+        {
+            return System.IO.DriveInfo.GetDrives()
+                .Where(d => d.DriveType == System.IO.DriveType.Fixed)
+                .Where(p => !p.RootDirectory.FullName.StartsWith("/private", StringComparison.OrdinalIgnoreCase))
+                .Select(d => d.RootDirectory.FullName)
+                .ToArray();
+        }
+    }
 }
