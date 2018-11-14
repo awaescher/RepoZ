513b9c35793f4acca3ba06ba990f77621517dc15
Andreas Wäscher <awaescher@sodacore.net>
2018-11-14 23:40:44 +0100

    Increase version to 4.0

--- RepoZ.Api/Properties/AssemblyInfo.cs
+++ RepoZ.Api/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("3.3.0.0")]
-[assembly: AssemblyVersion("3.3.0.0")]
-[assembly: AssemblyFileVersion("3.3.0.0")]
+// [assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
