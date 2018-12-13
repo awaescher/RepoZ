32ad74c1787b480db38718584ea8fa2bb92a1308
Andreas Wäscher <awaescher@sodacore.net>
2018-12-13 22:47:24 +0100

    Add humanizer for "last update" tooltips

---
+++ RepoZ.App.Win/Converters/UtcToLocalDateTimeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RepoZ.App.Win.Converters
+{
+	public class UtcToLocalDateTimeConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return DateTime.SpecifyKind(DateTime.Parse(value.ToString()), DateTimeKind.Utc).ToLocalTime();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
