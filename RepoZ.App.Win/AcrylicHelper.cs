c4556a2bf17b0995cfaf5769d5bc59d246ea0f82
Andreas Wäscher <awaescher@sodacore.net>
2019-03-06 00:19:52 +0100

    Add acrylic menus

---
+++ RepoZ.App.Win/AcrylicHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace RepoZ.App.Win
+{
+	public static class AcrylicHelper
+	{
+		public static void EnableBlur(Visual visual)
+		{
+			var hwnd = (HwndSource)PresentationSource.FromVisual(visual);
+			WindowsCompositionHelper.EnableBlur(hwnd.Handle);
+		}
+
+		public static void EnableBlurForMenu(Control menu)
+		{
+			menu.Background = new SolidColorBrush(Color.FromArgb(60, 0, 0, 0));
+			EnableBlur(menu);
+		}
+	}
+}
---
+++ RepoZ.App.Win/WindowsCompositionHelper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Drawing.Printing;
+using System.Runtime.InteropServices;
+
+namespace RepoZ.App.Win
+{
+	public static class WindowsCompositionHelper
+	{
+		[DllImport("user32.dll")]
+		private static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
+		[DllImport("dwmapi.dll", PreserveSig = true)]
+		private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+		[DllImport("dwmapi.dll")]
+		private static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref Margins pMarInset);
+
+		public static void EnableBlur(IntPtr hwnd)
+		{
+			try
+			{
+				var accent = new AccentPolicy();
+				var accentStructSize = Marshal.SizeOf(accent);
+				accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
+
+				var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+				Marshal.StructureToPtr(accent, accentPtr, false);
+
+				var data = new WindowCompositionAttributeData
+				{
+					Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY,
+					SizeOfData = accentStructSize,
+					Data = accentPtr
+				};
+
+				SetWindowCompositionAttribute(hwnd, ref data);
+
+				Marshal.FreeHGlobal(accentPtr);
+			}
+			catch (Exception)
+			{
+
+			}
+		}
+
+		public static bool EnableDropShadow(IntPtr hwnd, Margins margins)
+		{
+			try
+			{
+				int val = 2;
+				int ret1 = DwmSetWindowAttribute(hwnd, 2, ref val, 4);
+
+				if (ret1 == 0)
+				{
+					Margins m = new Margins { Bottom = 0, Left = 0, Right = 0, Top = 0 };
+					int ret2 = DwmExtendFrameIntoClientArea(hwnd, ref m);
+					return ret2 == 0;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+	}
+
+	internal enum AccentState
+	{
+		ACCENT_DISABLED = 1,
+		ACCENT_ENABLE_GRADIENT = 0,
+		ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+		ACCENT_ENABLE_BLURBEHIND = 3,
+		ACCENT_INVALID_STATE = 4
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	internal struct AccentPolicy
+	{
+		public AccentState AccentState;
+		public int AccentFlags;
+		public int GradientColor;
+		public int AnimationId;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	internal struct WindowCompositionAttributeData
+	{
+		public WindowCompositionAttribute Attribute;
+		public IntPtr Data;
+		public int SizeOfData;
+	}
+
+	internal enum WindowCompositionAttribute
+	{
+		WCA_ACCENT_POLICY = 19
+	}
+}
