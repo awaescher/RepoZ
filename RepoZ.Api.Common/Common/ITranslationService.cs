7bccda996483851e506a7ae6a1590783d3973fda
pedoc <pedoc@qq.com>
2019-09-12 11:45:35 +0800

    Translate the remaining strings to Chinese

--- RepoZ.Api.Common/Common/ITranslationService.cs
+++ RepoZ.Api.Common/Common/ITranslationService.cs
@@ -7,5 +7,6 @@ namespace RepoZ.Api.Common.Common
 	public interface ITranslationService
 	{
 		string Translate(string value);
-	}
+        string Translate(string value, params object[] args);
+    }
 }
