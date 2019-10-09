9d1bbe2aca4ddfdcbac23d53edd6bd3966340b42
awaescher <awaescher@gmx.de>
2019-10-09 12:21:50 +0200

    Fallback to english for unknown cultures

--- RepoZ.App.Mac/Main.cs
+++ RepoZ.App.Mac/Main.cs
@@ -1,26 +1,35 @@
 ﻿using AppKit;
 using Foundation;
+using System.Globalization;
 using System.Threading;
 
 namespace RepoZ.App.Mac
 {
-	static class MainClass
-	{
-		static void Main(string[] args)
-		{
-			NSApplication.Init();
-			SetCurrentCulture();
-			NSApplication.Main(args);
-		}
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            SetCurrentCulture();
+            NSApplication.Main(args);
+        }
 
-		private static void SetCurrentCulture()
-		{
-			if ((NSLocale.PreferredLanguages?.Length ?? 0) == 0)
-				return;
+        private static void SetCurrentCulture()
+        {
+            if ((NSLocale.PreferredLanguages?.Length ?? 0) == 0)
+                return;
 
-			Thread.CurrentThread.CurrentCulture
-				= Thread.CurrentThread.CurrentUICulture
-				= new System.Globalization.CultureInfo(NSLocale.PreferredLanguages[0]);
-		}
-	}
+            try
+            {
+                Thread.CurrentThread.CurrentCulture
+                    = Thread.CurrentThread.CurrentUICulture
+                    = new CultureInfo(NSLocale.PreferredLanguages[0]);
+            }
+            catch (CultureNotFoundException)
+            {
+                // stick with english, then ...
+            }
+
+        }
+    }
 }
